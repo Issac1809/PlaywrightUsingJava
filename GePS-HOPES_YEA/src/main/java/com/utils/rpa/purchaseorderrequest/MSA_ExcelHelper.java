100d2b5735f256ce55cee89a650bfc93551241a5
Issac <issac1809@gmail.com>
2025-03-25 22:07:42 +0530

    Updated MSA_ExcelHelper

--- GePS-HOPES_YEA/src/main/java/com/utils/rpa/purchaseorderrequest/MSA_ExcelHelper.java
+++ GePS-HOPES_YEA/src/main/java/com/utils/rpa/purchaseorderrequest/MSA_ExcelHelper.java
@@ -8,26 +8,50 @@ import java.io.*;
 public class MSA_ExcelHelper {
 
     static Logger logger;
+    static double randomNumber;
 
-//TODO Constructor
+    //TODO Constructor
     public MSA_ExcelHelper() {
         this.logger = LoggerUtil.getLogger(MSA_ExcelHelper.class);
     }
 
-    public static void updateExcel(String filePath, int rowNum, int colNum, String newValue) {
-        try{
+    public static void updateExcel(String filePath) {
+        try {
             FileInputStream fileInputStream = new FileInputStream(filePath);
             Workbook workbook = new XSSFWorkbook(fileInputStream);
-            Sheet sheet = workbook.getSheetAt(0);
-            Row row = sheet.getRow(rowNum);
-            if (row == null) row = sheet.createRow(rowNum);
-            Cell cell = row.getCell(colNum, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-            cell.setCellValue(newValue);
 
-            FileOutputStream fos = new FileOutputStream(filePath);
-            workbook.write(fos);
+//TODO Update Sheet 1 (RequesterInput)
+            Sheet sheet1 = workbook.getSheet("RequesterInput");
+            randomNumber = Math.round(Math.random() * 1000);
+            updateCell(sheet1, 4, 1, String.valueOf(randomNumber));
+            updateCell(sheet1, 4, 2, "POReleasePending");
+
+//TODO Update Sheet 2 (MSA)
+            Sheet sheet2 = workbook.getSheet("MSA");
+            int rowCount = sheet2.getLastRowNum(); //Get Total row count
+            for (int i = 1; i <= rowCount; i++) {
+                updateCell(sheet2, i, 11, String.valueOf(randomNumber)); //Tokuchu Number L column (index 11)
+                updateCell(sheet2, i, 12, String.valueOf(randomNumber)); //Tokuchu Number for Price M column (index 12)
+                updateCell(sheet2, i, 92, String.valueOf(randomNumber)); //PR Number CQ column (index 92)
+                updateCell(sheet2, i, 93, String.valueOf(randomNumber)); //PR Item Number CR column (index 93)
+                updateCell(sheet2, i, 94, String.valueOf(randomNumber)); //PO Number CS column (index 94)
+            }
+
+//TODO Save Changes
+            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+            workbook.write(fileOutputStream);
         } catch (IOException exception) {
             logger.error("Exception in update excel function: {}", exception.getMessage());
+        }
+    }
+
+    public static void updateCell(Sheet sheet, int rowNum, int colNum, String newValue) {
+        try {
+            Row row = sheet.getRow(rowNum);
+            Cell cell = row.getCell(colNum);
+            cell.setCellValue(newValue);
+        } catch (Exception exception) {
+            logger.error("Exception in update cell function: {}", exception.getMessage());
         }
     }
 }
